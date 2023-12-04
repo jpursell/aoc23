c00648588cfd31fdc78db25918c5cbb32664f174
Jeff Pursell <jpursell@gmail.com>
2023-12-04 08:44:33 -0500

    Add boilerplate

---
+++ src/day_5.rs
@@ -0,0 +1,23 @@
+pub fn day_5() {
+    // let input = include_str!("day_5_data.txt");
+    // println!("day 5a {}", day_5a(input));
+    // println!("day 5b {}", day_5b(input));
+}
+
+fn day_5a(input: &str) -> u32 {
+    0
+}
+
+fn day_5b(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test1() {
+        let input = r#""#;
+        assert_eq!(super::day_5a(input), 0);
+        assert_eq!(super::day_5b(input), 0);
+    }
+}
