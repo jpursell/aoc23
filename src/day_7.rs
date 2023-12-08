baf4ff48057a1df44e3b73d0e971079f2732e233
Jeff Pursell <jpursell@gmail.com>
2023-12-08 18:22:18 -0500

    Add day 7 stub

---
+++ src/day_7.rs
@@ -0,0 +1,23 @@
+pub fn day_7() {
+    // let input = include_str!("day_7_data.txt");
+    // println!("day 7 a {}", day_7_a(input));
+    // println!("day 7 b {}", day_7_b(input));
+}
+
+fn day_7_a(input: &str) -> u32 {
+    0
+}
+
+fn day_7_b(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test1() {
+        let input = r#""#;
+        assert_eq!(super::day_7_a(input), 0);
+        assert_eq!(super::day_7_b(input), 0);
+    }
+}
