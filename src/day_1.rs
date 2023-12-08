364e9ca4ea5ed35980534261dca2d36882ec2457
Jeff Pursell <jpursell@gmail.com>
2023-12-08 06:53:38 -0500

    Remove extra day 1a code

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,38 +1,7 @@
-use rayon::iter::{ParallelBridge, ParallelIterator};
-
 pub fn day_1() {
     let day_1_data = include_str!("day_1_data.txt");
-    println!("day 1a {}", day_1a(day_1_data));
+    println!("day 1a {}", day_1a_faster_fc(day_1_data));
     println!("day 1b {}", day_1b(day_1_data));
-}
-
-fn day_1a(input: &str) -> u32 {
-    fn find_first(line: &str) -> char {
-        for c in line.chars() {
-            if c.is_numeric() {
-                return c;
-            }
-        }
-        panic!("Did not find char");
-    }
-    fn find_last(line: &str) -> char {
-        for c in line.chars().rev() {
-            if c.is_numeric() {
-                return c;
-            }
-        }
-        panic!("Did not find char");
-    }
-    let mut sum: u32 = 0;
-    for line in input.split("\n") {
-        let first_digit = find_first(line);
-        let last_digit = find_last(line);
-        let num = format!("{}{}", first_digit, last_digit)
-            .parse::<u32>()
-            .unwrap();
-        sum += num;
-    }
-    sum
 }
 
 fn day_1a_proc_line(line: &str) -> u32 {
@@ -134,109 +103,6 @@ fn day_1a_faster_fc(input: &str) -> u32 {
         sum += day_1a_proc_line(line);
     }
     sum
-}
-
-fn day_1a_faster(input: &str) -> u32 {
-    let mut sum: u32 = 0;
-    for line in input.split("\n") {
-        for c in line.chars() {
-            match c {
-                '0' => {
-                    break;
-                }
-                '1' => {
-                    sum += 10;
-                    break;
-                }
-                '2' => {
-                    sum += 20;
-                    break;
-                }
-                '3' => {
-                    sum += 30;
-                    break;
-                }
-                '4' => {
-                    sum += 40;
-                    break;
-                }
-                '5' => {
-                    sum += 50;
-                    break;
-                }
-                '6' => {
-                    sum += 60;
-                    break;
-                }
-                '7' => {
-                    sum += 70;
-                    break;
-                }
-                '8' => {
-                    sum += 80;
-                    break;
-                }
-                '9' => {
-                    sum += 90;
-                    break;
-                }
-                _ => (),
-            }
-        }
-        for c in line.chars().rev() {
-            match c {
-                '0' => {
-                    break;
-                }
-                '1' => {
-                    sum += 1;
-                    break;
-                }
-                '2' => {
-                    sum += 2;
-                    break;
-                }
-                '3' => {
-                    sum += 3;
-                    break;
-                }
-                '4' => {
-                    sum += 4;
-                    break;
-                }
-                '5' => {
-                    sum += 5;
-                    break;
-                }
-                '6' => {
-                    sum += 6;
-                    break;
-                }
-                '7' => {
-                    sum += 7;
-                    break;
-                }
-                '8' => {
-                    sum += 8;
-                    break;
-                }
-                '9' => {
-                    sum += 9;
-                    break;
-                }
-                _ => (),
-            }
-        }
-    }
-    sum
-}
-
-fn day_1a_faster_par(input: &str) -> u32 {
-    input
-        .split("\n")
-        .par_bridge()
-        .map(|line| day_1a_proc_line(line))
-        .sum()
 }
 
 fn day_1b(input: &str) -> u32 {
@@ -443,25 +309,9 @@ zoneight234
         let day_1_data = include_str!("day_1_data.txt");
 
         let now = std::time::Instant::now();
-        assert_eq!(super::day_1a(day_1_data), 54644);
-        println!("day_1a took {} seconds", now.elapsed().as_secs_f32());
-
-        let now = std::time::Instant::now();
-        assert_eq!(super::day_1a_faster(day_1_data), 54644);
-        println!("day_1a_faster took {} seconds", now.elapsed().as_secs_f32());
-
-        let now = std::time::Instant::now();
         assert_eq!(super::day_1a_faster_fc(day_1_data), 54644);
         println!(
             "day_1a_faster_fc took {} seconds",
-            now.elapsed().as_secs_f32()
-        );
-
-        // slower on my dual core mac, might be too little work to benefit from parallelization
-        let now = std::time::Instant::now();
-        assert_eq!(super::day_1a_faster_par(day_1_data), 54644);
-        println!(
-            "day_1a_faster_par took {} seconds",
             now.elapsed().as_secs_f32()
         );
 
