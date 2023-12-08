c4976e8d01abb6df95c72cae16ca5570c0851a46
Jeff Pursell <jpursell@gmail.com>
2023-12-08 06:55:52 -0500

    Add day 6 stub

---
+++ src/day_6.rs
@@ -0,0 +1,23 @@
+pub fn day_6() {
+    // let input = include_str!("day_6_data.txt");
+    // println!("day n a {}", day_6_a(input));
+    // println!("day n b {}", day_6_b(input));
+}
+
+fn day_6_a(input: &str) -> u32 {
+    0
+}
+
+fn day_6_b(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test1() {
+        let input = r#""#;
+        assert_eq!(super::day_6_a(input), 0);
+        assert_eq!(super::day_6_b(input), 0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 fn main() {
     day_1::day_1();
@@ -10,4 +11,5 @@ fn main() {
     day_3::day_3();
     day_4::day_4();
     day_5::day_5();
+    day_6::day_6();
 }
