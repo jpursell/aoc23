474e4941374157807e8057f6aacbc6a190007733
Jeff Pursell <jpursell@gmail.com>
2023-12-08 18:05:41 -0500

    Finish 6a

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,15 +1,36 @@
 use std::str::FromStr;
 
 pub fn day_6() {
-    // let input = include_str!("day_6_data.txt");
-    // println!("day n a {}", day_6_a(input));
-    // println!("day n b {}", day_6_b(input));
+    let input = include_str!("day_6_data.txt");
+    println!("day 6 a {}", day_6_a(input));
+    // println!("day 6 b {}", day_6_b(input));
 }
 
 #[derive(Debug)]
 struct Race {
     time: u64,
     distance: u64,
+}
+
+impl Race {
+    fn num_ways_to_win(&self) -> u64 {
+        // this is just the quadratic formula with
+        // a == 1, b == -t, c == d
+        let d = self.distance as f32;
+        let t = self.time as f32;
+        let inner = (t * t - 4.0 * d).sqrt();
+        let low = (t - inner) / 2.0;
+        let mut low_ceil = low.ceil();
+        if low == low_ceil {
+            low_ceil += 1.0;
+        }
+        let high = (t + inner) / 2.0;
+        let mut high_floor = high.floor();
+        if  high_floor == high {
+            high_floor -= 1.0;
+        }
+        high_floor as u64 - low_ceil as u64 + 1
+    }
 }
 
 #[derive(Debug)]
@@ -57,15 +78,18 @@ impl FromStr for RaceHistory {
 impl RaceHistory {
     fn num_ways_to_win(&self) -> u64 {
         // TODO finish this statement
-        self.races.map(|race| race.num_ways_to_win())
+        self.races
+            .iter()
+            .map(|race| race.num_ways_to_win())
+            .product()
     }
 }
 
 fn day_6_a(input: &str) -> u64 {
-    dbg!(input
+    input
         .parse::<RaceHistory>()
-        .expect("Failed to parse RaceHistory"))
-    .num_ways_to_win()
+        .expect("Failed to parse RaceHistory")
+        .num_ways_to_win()
 }
 
 fn day_6_b(input: &str) -> u32 {
@@ -79,6 +103,6 @@ mod tests {
         let input = r#"Time:      7  15   30
 Distance:  9  40  200"#;
         assert_eq!(super::day_6_a(input), 288);
-        assert_eq!(super::day_6_b(input), 0);
+        assert_eq!(super::day_6_b(input), 71503);
     }
 }
