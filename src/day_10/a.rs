5aa77f88e9717b12f5f6f9db76ce4112a1113a8c
Jeffrey Pursell <Jeffrey.Pursell@maxar.com>
2023-12-12 15:49:01 -0500

    Work on 10 a

--- src/day_10/a.rs
+++ src/day_10/a.rs
@@ -36,7 +36,7 @@ enum Direction {
     W,
 }
 
-fn opposite_direction(direction:Direction) -> Direction {
+fn opposite_direction(direction: &Direction) -> Direction {
     match direction {
         Direction::N => Direction::S,
         Direction::E => Direction::W,
@@ -45,7 +45,7 @@ fn opposite_direction(direction:Direction) -> Direction {
     }
 }
 
-fn get_directions(c: char) -> Option<(Direction, Direction)> {
+fn get_directions(c: &char) -> Option<(Direction, Direction)> {
     match c {
         '|' => Some((Direction::N, Direction::S)),
         '-' => Some((Direction::E, Direction::W)),
@@ -54,18 +54,22 @@ fn get_directions(c: char) -> Option<(Direction, Direction)> {
         '7' => Some((Direction::S, Direction::W)),
         'F' => Some((Direction::S, Direction::E)),
         '.' => None,
-        _ => panic!()
+        _ => panic!(),
     }
 }
 
-fn tile_supports_dir(tile: char, direction: Direction) -> bool {
-    let Some((d1, d2)) = get_directions(tile) else {return false;};
-    if d1 == direction || d2 == direction {return true;}
+fn tile_supports_dir(tile: &char, direction: &Direction) -> bool {
+    let Some((d1, d2)) = get_directions(tile) else {
+        return false;
+    };
+    if d1 == *direction || d2 == *direction {
+        return true;
+    }
     false
 }
 
-fn can_go_to_tile(tile: char, direction: Direction) -> bool {
-    tile_supports_dir(tile, opposite_direction(direction))
+fn can_go_to_tile(tile: &char, direction: &Direction) -> bool {
+    tile_supports_dir(tile, &opposite_direction(direction))
 }
 
 struct MazeWalker<'a> {
@@ -75,68 +79,86 @@ struct MazeWalker<'a> {
 }
 
 impl<'a> MazeWalker<'a> {
-    fn get_tile(&self, direction:Direction) -> Option<char> {
+    fn get_tile(&self, direction: &Direction) -> Option<char> {
         match direction {
             Direction::N => {
-                if self.pos.0 == 0 {return None}
-                return Some(self.maze.map[self.pos.0 - 1][self.pos.1])
+                if self.pos.0 == 0 {
+                    return None;
+                }
+                return Some(self.maze.map[self.pos.0 - 1][self.pos.1]);
             }
             Direction::E => {
-                if self.pos.1 == self.maze.ncols - 1 {return None}
-                return Some(self.maze.map[self.pos.0][self.pos.1 + 1])
+                if self.pos.1 == self.maze.ncols - 1 {
+                    return None;
+                }
+                return Some(self.maze.map[self.pos.0][self.pos.1 + 1]);
             }
             Direction::S => {
-                if self.pos.0 == self.maze.nrows - 1 {return None}
-                return Some(self.maze.map[self.pos.0 + 1][self.pos.1])
+                if self.pos.0 == self.maze.nrows - 1 {
+                    return None;
+                }
+                return Some(self.maze.map[self.pos.0 + 1][self.pos.1]);
             }
             Direction::W => {
-                if self.pos.1 == 0 {return None}
-                return Some(self.maze.map[self.pos.0][self.pos.1 - 1])
+                if self.pos.1 == 0 {
+                    return None;
+                }
+                return Some(self.maze.map[self.pos.0][self.pos.1 - 1]);
             }
         }
     }
 
-    fn can_move(&self, direction: Direction) -> bool {
-        let Some(tile) = self.get_tile(direction) else {return false;};
-        can_go_to_tile(tile, direction)
+    fn can_move(&self, direction: &Direction) -> bool {
+        let Some(tile) = self.get_tile(direction) else {
+            return false;
+        };
+        can_go_to_tile(&tile, direction)
     }
 
-    fn directions(&self, pos: (usize, usize)) -> Vec<Directions> {
-        let current_char = self.map[pos.0][pos.1];
-        if (current_char == 'S') {
-            vec![Direction::N, Direction::E, Direction::S, Direction::W].iter().filter(|d| self.can_move())
+    fn new(maze: &Maze) -> MazeWalker {
+        let pos = maze.find_start();
+        MazeWalker {
+            maze,
+            pos: maze.find_start().expect("No start!"),
+            last_direction: None,
         }
     }
 
-    fn new(maze: &maze) -> MazeWalker {
-        let pos = maze.find_start();
-        MazeWalker{maze,pos: maze.find_start(), last_direction: None}
-    }
-
-    fn make_move(&mut self, direction: Direction) {
+    fn advance_position(&mut self, direction: &Direction) {
         self.pos = match direction {
             Direction::N => (self.pos.0 - 1, self.pos.1),
             Direction::E => (self.pos.0, self.pos.1 + 1),
             Direction::S => (self.pos.0 + 1, self.pos.1),
             Direction::W => (self.pos.0, self.pos.1 - 1),
         };
-        self.last_direction = Some(direction);
+        self.last_direction = Some(*direction);
     }
 
-    fn find_move(&mut self) -> Result<(), ()> {
+    fn make_move(&mut self) -> Result<(), ()> {
         match self.last_direction {
-            Some(ld) => todo!()
+            Some(ld) => {
+                let tile = &self.maze.map[self.pos.0][self.pos.1];
+                let directions = get_directions(tile).expect("Not on a valid tile!");
+                let direction = if directions.0 == ld {
+                    directions.1
+                } else if directions.1 == ld {
+                    directions.0
+                } else {
+                    panic!()
+                };
+                assert!(self.can_move(&direction));
+                self.advance_position(&direction);
+            }
             None => {
                 for dir in Direction::iter() {
-                    if self.can_move(dir) {
-                        self.make_move(dir);
+                    if self.can_move(&dir) {
+                        self.advance_position(&dir);
                     }
                 }
             }
         }
-        OK(())
+        Ok(())
     }
-
 }
 
 impl Maze {
@@ -144,7 +166,7 @@ impl Maze {
         for (irow, row) in self.map.iter().enumerate() {
             for (icol, col) in row.iter().enumerate() {
                 if *col == 'S' {
-                return  Ok((irow, icol));
+                    return Ok((irow, icol));
                 }
             }
         }
@@ -153,10 +175,10 @@ impl Maze {
 
     fn count_steps(&self) -> u64 {
         let mut pos = self.find_start();
-        let walker = MazeWalker{self, pos};
+        let walker = MazeWalker::new(self);
+        todo!();
         0
     }
-
 }
 
 #[cfg(test)]
