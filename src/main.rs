5956a41e62359877adbc0ddfe00b8a65380d1cc9
Jeff Pursell <jpursell@gmail.com>
2023-12-11 19:20:01 -0500

    Add arg parse

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,39 @@ mod day_7;
 mod day_8;
 
 fn main() {
-    day_1::day_1();
-    day_2::day_2();
-    day_3::day_3();
-    day_4::day_4();
-    day_5::day_5();
-    day_6::day_6();
-    day_7::day_7();
-    day_8::run();
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        println!("Usage: {} NUM", args[0]);
+        return;
+    }
+    let num = args[1].parse::<u8>().unwrap();
+    match num {
+        1 => {
+            day_1::day_1();
+        }
+        2 => {
+            day_2::day_2();
+        }
+        3 => {
+            day_3::day_3();
+        }
+        4 => {
+            day_4::day_4();
+        }
+        5 => {
+            day_5::day_5();
+        }
+        6 => {
+            day_6::day_6();
+        }
+        7 => {
+            day_7::day_7();
+        }
+        8 => {
+            day_8::run();
+        }
+        _ => {
+            panic!();
+        }
+    }
 }
