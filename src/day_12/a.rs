e511a95ce88523dee42189c51f2033cdab098b67
Jeff Pursell <jpursell@gmail.com>
2023-12-13 19:17:09 -0500

    Start 12 a

--- src/day_12/a.rs
+++ src/day_12/a.rs
@@ -1,5 +1,61 @@
-pub fn run(_input: &str) -> u64 {
-    0
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Condition {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
+impl TryFrom<char> for Condition {
+    type Error = &'static str;
+    fn try_from(c: char) -> Result<Self, <Self as TryFrom<char>>::Error> {
+        match c {
+            '.' => Ok(Condition::Operational),
+            '#' => Ok(Condition::Damaged),
+            '?' => Ok(Condition::Unknown),
+            _ => Err("Unknown char"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SpringRecord {
+    record: Vec<Condition>,
+    groups: Vec<u32>,
+}
+
+impl FromStr for SpringRecord {
+    type Err = &'static str;
+    fn from_str(line: &str) -> Result<Self, <Self as FromStr>::Err> {
+        let (record, groups) = line.split_once(" ").unwrap();
+        let record = record
+            .chars()
+            .map(|c| Condition::try_from(c).unwrap())
+            .collect::<Vec<_>>();
+        let groups = groups
+            .split(",")
+            .map(|num| num.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        Ok(SpringRecord { record, groups })
+    }
+}
+
+impl SpringRecord {
+    fn count_solutions(&self) -> u64 {
+        0
+    }
+}
+
+pub fn run(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| line.parse::<SpringRecord>().unwrap())
+        .inspect(|sr| {
+            dbg!(&sr);
+        })
+        .map(|sr| sr.count_solutions())
+        .sum()
 }
 
 #[cfg(test)]
@@ -7,6 +63,6 @@ mod tests {
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input), 0);
+        assert_eq!(super::run(input), 21);
     }
 }
