e33e8851a507a90f0203433bf14cde8b064985ea
Jeff Pursell <jpursell@gmail.com>
2023-12-16 18:51:25 -0500

    Start on 13 a parsing

--- src/day_13/a.rs
+++ src/day_13/a.rs
@@ -1,3 +1,46 @@
+use std::str::FromStr;
+
+#[derive(Hash)]
+enum Pix {
+    Roc,
+    Ash,
+}
+
+impl TryFrom<char> for Pix {
+    type Error = &'static str;
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '.' => Pix::Ash,
+            '#' => Pix::Roc,
+            _ => "Invalid char",
+        }
+    }
+}
+
+struct Pattern {
+    pix: Vec<Vec<Pix>>,
+}
+
+struct Patterns {
+    patterns: Vec<Patter>,
+}
+
+impl FromStr for Patters {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut patterns = Vec::new();
+        let lines = Vec::new();
+        for line in s.lines() {
+            if line.len() > 0 {
+                lines.push(line);
+            } else {
+                patterns.push(lines.join("\n").parse<Patter>().unwrap());
+            }
+        }
+        Ok(Patters{patterns})
+    }
+}
+
 pub fn run(_input: &str) -> u64 {
     0
 }
