341f1f943a1c96640bc729eb1fc75e707d67bcb3
Jeff Pursell <jpursell@gmail.com>
2023-12-19 05:23:37 -0500

    Work on 14 b

--- src/day_14/b.rs
+++ src/day_14/b.rs
@@ -1,11 +1,13 @@
+use ndarray::{Array2, ArrayViewMut1, Axis};
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Rock {
     R,
     S,
     N,
 }
+
 impl TryFrom<char> for Rock {
     type Error = &'static str;
     fn try_from(value: char) -> Result<Self, Self::Error> {
@@ -17,9 +19,14 @@ impl TryFrom<char> for Rock {
         }
     }
 }
+
+#[derive(PartialEq, Debug)]
 struct RockField {
-    rocks: Vec<Vec<Rock>>,
+    rocks: Array2<Rock>,
+    nrows: usize,
+    // ncols: usize,
 }
+
 impl FromStr for RockField {
     type Err = &'static str;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -31,49 +38,118 @@ impl FromStr for RockField {
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
-        Ok(RockField { rocks })
+        let nrows = rocks.len();
+        let ncols = rocks[0].len();
+        let rocks = rocks.concat();
+        let rocks = Array2::from_shape_vec((nrows, ncols), rocks).unwrap();
+        Ok(RockField {
+            rocks,
+            nrows,
+            // ncols,
+        })
     }
 }
-#[derive(Debug, PartialEq, Eq)]
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Direction {
     N,
     E,
     S,
     W,
 }
+
 impl RockField {
-    fn roll(&mut self, direction: Direction) -> bool {
-        assert_eq!(direction, Direction::N);
-        let mut changed = false;
-        for irow in 1..self.rocks.len() {
-            for icol in 0..self.rocks[0].len() {
-                if self.rocks[irow][icol] == Rock::R && self.rocks[irow - 1][icol] == Rock::N {
-                    self.rocks[irow][icol] = Rock::N;
-                    self.rocks[irow - 1][icol] = Rock::R;
-                    changed = true
+    fn roll_arr(arr: &mut ArrayViewMut1<Rock>, forward: bool) {
+        if forward {
+            // if forward then move rocks to higher index
+            let mut write = arr.len() - 1;
+            for read in (0..arr.len()).rev() {
+                if arr[read] == Rock::S {
+                    write = read;
+                }
+                while write > 0 && arr[write] != Rock::N {
+                    write -= 1;
+                }
+                if read >= write || arr[write] != Rock::N {
+                    continue;
+                }
+                if arr[read] == Rock::R {
+                    arr[read] = Rock::N;
+                    arr[write] = Rock::R;
+                }
+            }
+        } else {
+            // if !forward then move rocks to lower index
+            let mut write = 0;
+            for read in 0..arr.len() {
+                if arr[read] == Rock::S {
+                    write = read;
+                }
+                while write < arr.len() - 1 && arr[write] != Rock::N {
+                    write += 1;
+                }
+                if read <= write || arr[write] != Rock::N {
+                    continue;
+                }
+                if arr[read] == Rock::R {
+                    arr[read] = Rock::N;
+                    arr[write] = Rock::R;
                 }
             }
         }
-        changed
+    }
+    fn roll(&mut self, direction: Direction) {
+        match direction {
+            Direction::S => {
+                self.rocks
+                    .axis_iter_mut(Axis(1))
+                    .for_each(|mut c| RockField::roll_arr(&mut c, true));
+            }
+            Direction::N => {
+                self.rocks
+                    .axis_iter_mut(Axis(1))
+                    .for_each(|mut c| RockField::roll_arr(&mut c, false));
+            }
+            Direction::E => {
+                self.rocks
+                    .axis_iter_mut(Axis(0))
+                    .for_each(|mut c| RockField::roll_arr(&mut c, true));
+            }
+            Direction::W => {
+                self.rocks
+                    .axis_iter_mut(Axis(0))
+                    .for_each(|mut c| RockField::roll_arr(&mut c, false));
+            }
+        }
     }
     fn count_rocks(&self) -> usize {
-        let nrows = self.rocks.len();
         self.rocks
-            .iter()
-            .enumerate()
-            .map(|(irow, row)| {
-                row.iter()
-                    .map(|r| if *r == Rock::R { nrows - irow } else { 0 })
-                    .sum::<usize>()
-            })
+            .indexed_iter()
+            .map(
+                |((irow, _), r)| {
+                    if *r == Rock::R {
+                        self.nrows - irow
+                    } else {
+                        0
+                    }
+                },
+            )
             .sum::<usize>()
     }
 }
 pub fn run(input: &str) -> usize {
     let mut field = input.parse::<RockField>().unwrap();
-    loop {
-        if !field.roll(Direction::N) {
-            break;
+    let directions = vec![Direction::N, Direction::W, Direction::S, Direction::E];
+    // let ncycles = 1_000_000_000;
+    for cycle in 0..100 {
+        for direction in &directions {
+            field.roll(*direction);
+        }
+        let n = field.count_rocks();
+        if n == 64 {
+            println!("{} {} <=====", cycle, field.count_rocks());
+        } else {
+            println!("{} {}", cycle, field.count_rocks());
         }
     }
     field.count_rocks()
@@ -81,9 +157,27 @@ pub fn run(input: &str) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use super::{Direction, RockField};
+
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input), 136);
+        assert_eq!(super::run(input), 64);
+    }
+    #[test]
+    fn roll_test() {
+        let input = include_str!("example_data.txt");
+        let mut field = input.parse::<RockField>().unwrap();
+        field.roll(Direction::N);
+        let expected = include_str!("example_data_n.txt");
+        let expected = expected.parse::<RockField>().unwrap();
+        assert_eq!(field, expected);
+    }
+    #[test]
+    fn test_a() {
+        let input = include_str!("example_data.txt");
+        let mut field = input.parse::<RockField>().unwrap();
+        field.roll(Direction::N);
+        assert_eq!(field.count_rocks(), 136);
     }
 }
