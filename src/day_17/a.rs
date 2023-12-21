810035ac871865422f84a3daf57b95f885e7b321
Jeff Pursell <jpursell@gmail.com>
2023-12-21 06:38:24 -0500

    Work on 17 a

--- src/day_17/a.rs
+++ src/day_17/a.rs
@@ -1,3 +1,55 @@
+use std::str::FromStr;
+
+use ndarray::Array2;
+
+struct Position {
+    row: u8,
+    col: u8,
+}
+struct Info {
+    last_position: Position,
+    last_straight: u8,
+    loss: u16,
+}
+struct Solver {
+    visited: Array2<bool>,
+    table: Array2<Info>,
+    nrows: u16,
+    ncols: u16,
+}
+impl From<LossMap> for Solver {
+    fn from(loss_map: LossMap) -> Self {
+        let (nrows, ncols) = (loss_map.nrows, loss_map.ncols);
+        let visited = Array2::<bool>::from_elem((nrows, ncols), false);
+        todo!()
+    }
+}
+struct LossMap {
+    data: Array2<u8>,
+    nrows: u16,
+    ncols: u16,
+}
+impl FromStr for LossMap {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let data = s
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|c| c.to_digit(10).unwrap() as u8)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        let nrows = data.len();
+        let ncols = data[0].len();
+        let data = Array2::from_shape_vec((nrows, ncols), data.concat()).unwrap();
+        Ok(LossMap {
+            data,
+            nrows: nrows as u16,
+            ncols: ncols as u16,
+        })
+    }
+}
 pub fn run(_input: &str) -> usize {
     0
 }
