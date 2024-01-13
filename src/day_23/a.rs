20177b1903f287d999dd7a0f1388e1cef0bba2ca
Jeff Pursell <jpursell@gmail.com>
2024-01-13 16:01:21 -0500

    Work on 23 a

--- src/day_23/a.rs
+++ src/day_23/a.rs
@@ -1,3 +1,67 @@
+use std::str::FromStr;
+
+use ndarray::Array2;
+
+struct Node {
+    row: usize,
+    col: usize,
+}
+struct Edge {
+    start: Node,
+    end: Node,
+}
+struct Graph {
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+}
+
+#[derive(Clone, Copy)]
+enum MapSymbol {
+    Forest,
+    Path,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl TryFrom<char> for MapSymbol {
+    type Error = &'static str;
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '#' => Ok(MapSymbol::Forest),
+            '.' => Ok(MapSymbol::Path),
+            '^' => Ok(MapSymbol::Up),
+            'v' => Ok(MapSymbol::Down),
+            '<' => Ok(MapSymbol::Left),
+            '>' => Ok(MapSymbol::Right),
+            _ => Err("Unknown symbol"),
+        }
+    }
+}
+
+impl FromStr for Graph {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let map = s
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|c| MapSymbol::try_from(c).unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        let nrows = map.len();
+        let ncols = map[0].len();
+        todo!() // finish parsing graph
+        let map = Array2::from_shape_vec((nrows, ncols), map.concat());
+        let edges = Vec::new();
+        let nodes = Vec::new();
+
+        Ok(Graph{edges, nodes})
+    }
+}
+
 pub fn run(_input: &str) -> usize {
     0
 }
@@ -7,6 +71,6 @@ mod tests {
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input), 0);
+        assert_eq!(super::run(input), 94);
     }
 }
