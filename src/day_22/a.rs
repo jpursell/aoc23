063185b292daf0483d9191d36b201ef66b567d8c
Jeff Pursell <jpursell@gmail.com>
2024-01-13 08:15:48 -0500

    Work on 22 a

--- src/day_22/a.rs
+++ src/day_22/a.rs
@@ -1,9 +1,95 @@
 use std::{fmt::Display, str::FromStr};
 
+use ndarray::{s, Array3, Dim, SliceInfo, SliceInfoElem};
+
 struct Brick {
     x: [u16; 2],
     y: [u16; 2],
     z: [u16; 2],
+}
+impl PartialEq for Brick {
+    fn eq(&self, other: &Self) -> bool {
+        self.z[0] == other.z[0]
+    }
+}
+impl PartialOrd for Brick {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Eq for Brick { }
+impl Ord for Brick {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.z[0].cmp(&other.z[0])
+    }
+}
+impl Brick {
+    fn new(mut x: [u16; 2], mut y: [u16; 2], mut z: [u16; 2]) -> Brick {
+        if x[0] > x[1] {
+            x.swap(0, 1);
+        }
+        if y[0] > y[1] {
+            y.swap(0, 1);
+        }
+        if z[0] > z[1] {
+            z.swap(0, 1);
+        }
+        Brick { x, y, z }
+    }
+    fn slice(&self) -> SliceInfo<[SliceInfoElem; 3], Dim<[usize; 3]>, Dim<[usize; 3]>> {
+        s![
+            self.x[0] as usize..self.x[1] as usize,
+            self.y[0] as usize..self.y[1] as usize,
+            self.z[0] as usize..self.z[1] as usize
+        ]
+    }
+    /// Return new brick with lower z value
+    fn drop(&self, amount: u16) -> Result<Brick, ()> {
+        if amount > self.z[0] {
+            Err(())
+        } else {
+            let x = self.x;
+            let y = self.y;
+            let z = [self.z[0] - amount, self.z[1] - amount];
+            Ok(Brick::new(x, y, z))
+        }
+    }
+    /// Return new brick with lower z value and a height of 1
+    fn drop_bottom_rim(&self, amount: u16) -> Result<Brick, ()> {
+        if amount > self.z[0] {
+            Err(())
+        } else {
+            let x = self.x;
+            let y = self.y;
+            let z = [self.z[0] - amount, self.z[0] - amount + 1];
+            Ok(Brick::new(x, y, z))
+        }
+    }
+    /// Return new brick with lower z value that does not overlap
+    fn drop_bottom(&self, amount: u16) -> Result<Brick, ()> {
+        if amount > self.z[0] {
+            Err(())
+        } else {
+            let height = (self.z[1] - self.z[0]).min(amount);
+            let x = self.x;
+            let y = self.y;
+            let z = [self.z[0] - amount, self.z[0] - amount + height];
+            Ok(Brick::new(x, y, z))
+        }
+    }
+    /// Return new brick that is remainder of self minus drop bottom
+    fn drop_top(&self, amount: u16) -> Result<Brick, ()> {
+        if amount > self.z[0] {
+            Err(())
+        } else {
+            let brick_height = self.z[1] - self.z[0];
+            let height = brick_height.min(amount);
+            let x = self.x;
+            let y = self.y;
+            let z = [self.z[1] - height, self.z[1]];
+            Ok(Brick::new(x, y, z))
+        }
+    }
 }
 /// Parse something like 1,0,1 => x,y,z
 fn parse_xyz(s: &str) -> Vec<u16> {
@@ -27,11 +113,11 @@ impl FromStr for Brick {
             } else {
                 let start = &start_end[0];
                 let end = &start_end[1];
-                Ok(Brick {
-                    x: [start[0], end[0] + 1],
-                    y: [start[1], end[1] + 1],
-                    z: [start[2], end[2] + 1],
-                })
+                Ok(Brick::new(
+                    [start[0], end[0] + 1],
+                    [start[1], end[1] + 1],
+                    [start[2], end[2] + 1],
+                ))
             }
         }
     }
@@ -55,6 +141,7 @@ impl Display for Brick {
 
 struct Bricks {
     bricks: Vec<Brick>,
+    has_brick: Array3<bool>,
 }
 impl FromStr for Bricks {
     type Err = &'static str;
@@ -76,36 +163,71 @@ impl Display for Bricks {
 }
 
 impl Bricks {
-    fn new(bricks: Vec<Brick>) ->Bricks{
-        let x_min = bricks[0].x[0];
-        let x_max = bricks[0].x[0];
-        todo!() 
-        // populate volume Array3<bool>
+    fn new(mut bricks: Vec<Brick>) -> Bricks {
+        bricks.sort();
+        let x_min = bricks.iter().map(|b| b.x[0]).min().unwrap();
+        let y_min = bricks.iter().map(|b| b.y[0]).min().unwrap();
+        let x_max = bricks.iter().map(|b| b.x[1]).max().unwrap();
+        let y_max = bricks.iter().map(|b| b.y[1]).max().unwrap();
+        let z_max = bricks.iter().map(|b| b.z[1]).max().unwrap();
+        let nx = x_max - x_min;
+        let ny = y_max - y_min;
+        let nz = z_max;
+        let mut has_brick = Array3::from_elem([nx as usize, ny as usize, nz as usize], false);
+        bricks.iter().for_each(|b| {
+            has_brick.slice_mut(b.slice()).map_inplace(|x| {
+                *x = true;
+            })
+        });
+        Bricks { bricks, has_brick }
     }
     /// Try to move a brick down from gravity. Return if something changed
-    fn settle_a_brick(&mut self) -> bool {
+    fn settle_bricks(&mut self) -> bool {
         let mut changed = false;
         for i in 0..self.bricks.len() {
-            if self.can_settle(i) {
-                self.perform_settle(i);
+            let amount = self.max_settle(i);
+            if amount > 0 {
+                self.perform_settle(i, amount);
                 changed = true;
-                break;
             }
         }
         changed
     }
     /// Determin if brick at i can settle
-    fn can_settle(&self, i: usize) -> bool {
-        todo!()
+    fn can_settle(&self, i: usize, amount: u16) -> bool {
+        if let Ok(dropped) = self.bricks[i].drop_bottom_rim(amount) {
+            self.has_brick.slice(dropped.slice()).iter().all(|x| !*x)
+        } else {
+            false
+        }
+    }
+    /// Determine max amount a brick can settle at position i
+    fn max_settle(&self, i: usize) -> u16 {
+        let mut max_settle = 0;
+        while self.can_settle(i, max_settle + 1) {
+            max_settle += 1;
+        }
+        max_settle
     }
     /// Settle brick at i
-    fn perform_settle(&mut self, i: usize) -> bool {
-        todo!()
+    fn perform_settle(&mut self, i: usize, amount: u16) {
+        self.has_brick
+            .slice_mut(self.bricks[i].drop_top(amount).unwrap().slice())
+            .map_inplace(|x| {
+                *x = false;
+            });
+        self.has_brick
+            .slice_mut(self.bricks[i].drop_bottom(amount).unwrap().slice())
+            .map_inplace(|x| {
+                *x = true;
+            });
+        *self.bricks.get_mut(i).unwrap() = self.bricks[i].drop(amount).unwrap();
+        self.bricks.sort();
     }
     /// Keep settling bricks untill no more can settle
     fn settle_all(&mut self) {
         loop {
-            let changed = self.settle_a_brick();
+            let changed = self.settle_bricks();
             if !changed {
                 break;
             }
