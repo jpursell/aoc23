03eac24abed085cc51a3e7cd6c8f5c9cb1c9304c
Jeff Pursell <jpursell@gmail.com>
2024-01-14 10:56:05 -0500

    Work on 24 a

--- src/day_24/a.rs
+++ src/day_24/a.rs
@@ -1,4 +1,22 @@
-pub fn run(_input: &str) -> usize {
+use std::str::FromStr;
+
+struct Position {
+    vec: [usize; 3],
+}
+struct Velocity {
+    vec: [i32; 3],
+}
+struct InitialCondition {
+    position: Position,
+    velocity: Velocity,
+}
+impl FromStr for InitialCondition {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+pub fn run(_input: &str, min_test: usize, max_test: usize) -> usize {
     0
 }
 
@@ -7,6 +25,6 @@ mod tests {
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input), 2);
+        assert_eq!(super::run(input, 7, 27), 2);
     }
 }
