7b9da0ed7488a169d4c77a3414a2fd83ef71f4b2
Jeff Pursell <jpursell@gmail.com>
2024-01-14 13:23:57 -0500

    Work on 24 a

--- src/day_24/a.rs
+++ src/day_24/a.rs
@@ -1,5 +1,6 @@
 use std::{fmt::Display, str::FromStr};
 
+#[derive(Clone, Copy)]
 struct Position {
     vec: [usize; 3],
 }
@@ -25,13 +26,22 @@ impl FromStr for Position {
         Ok(Position::new(vec))
     }
 }
+#[derive(Clone, Copy)]
 struct Velocity {
     vec: [i32; 3],
 }
 impl Velocity {
     fn new(vec: [i32; 3]) -> Velocity {
+        vec.iter().for_each(|x| {
+            assert_ne!(*x, 0);
+        });
         Velocity { vec }
     }
+    // fn norm_xy(&self) -> [f64; 2] {
+    //     let out = [self.vec[0] as f64, self.vec[1] as f64];
+    //     let norm = f64::sqrt(out[0].powi(2) + out[1].powi(2));
+    //     [out[0] / norm, out[1] / norm]
+    // }
 }
 impl Display for Velocity {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -50,6 +60,7 @@ impl FromStr for Velocity {
         Ok(Velocity::new(vec))
     }
 }
+#[derive(Clone, Copy)]
 struct InitialCondition {
     position: Position,
     velocity: Velocity,
@@ -74,12 +85,100 @@ impl Display for InitialCondition {
         write!(f, "{} @ {}", self.position, self.velocity)
     }
 }
+struct Line {
+    m: f64,
+    b: f64,
+}
+impl Line {
+    fn new(m: f64, b: f64) -> Line {
+        Line { m, b }
+    }
+}
+impl Display for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "y = {} x + {}", self.m, self.b)
+    }
+}
+impl TryFrom<InitialCondition> for Line {
+    type Error = ();
+    fn try_from(value: InitialCondition) -> Result<Self, Self::Error> {
+        // pos x,y moving at vel dx,dy
+        // y = mx + b
+        // when x is x we get
+        // y = b
+        let x = value.position.vec[0] as f64;
+        let y = value.position.vec[1] as f64;
+        let dx = value.velocity.vec[0] as f64;
+        let dy = value.velocity.vec[1] as f64;
+        let b = {
+            // we want to find the value for t when x is 0
+            // newx = x + dx * t
+            // -x = dx * t
+            // t = -x / dx
+            let t = -x / dx;
+            y + dy * t
+        };
+        if dx == 0_f64 {
+            return Err(());
+        }
+        let m = dy / dx;
+        Ok(Line::new(m, b))
+    }
+}
+fn find_intersection(a: &Line, b: &Line) -> Option<[f64; 2]> {
+    if a.m == b.m {
+        return None;
+    }
+    // y = mx + b
+    // m0 * x + b0 - m1 * x - b1 = 0
+    // m0 * x - m1 * x = b1 - b0
+    // x (m0 - m1) = b1 - b0
+    // x = (b1 - b0) / (m0 - m1)
+    let x = (b.b - a.b) / (a.m - b.m);
+    let y_a = a.m * x + a.b;
+    // let y_b = b.m * x + b.b;
+    // assert_eq!(y_a, y_b);
+    Some([x, y_a])
+}
 struct HailCloud {
     stones: Vec<InitialCondition>,
+    stone_lines: Vec<Line>,
 }
 impl HailCloud {
     fn new(stones: Vec<InitialCondition>) -> HailCloud {
-        HailCloud { stones }
+        let stone_lines = stones
+            .iter()
+            .map(|x| Line::try_from(*x).unwrap())
+            .collect::<Vec<_>>();
+        HailCloud {
+            stones,
+            stone_lines,
+        }
+    }
+    fn count_intersections_in_test_area(&self, min_test: f64, max_test: f64) -> usize {
+        let mut count = 0;
+        for i in 0..self.stones.len() {
+            for j in i + 1..self.stones.len() {
+                let intersection = find_intersection(&self.stone_lines[i], &self.stone_lines[j]);
+                if intersection.is_none() {
+                    continue;
+                }
+                println!(
+                    "\nintersect\n\t{}\n\t{}\n\t{:?}",
+                    self.stones[i], self.stones[j], intersection
+                );
+                let intersection = intersection.unwrap();
+                if intersection[0] >= min_test
+                    && intersection[0] <= max_test
+                    && intersection[1] >= min_test
+                    && intersection[1] <= max_test
+                {
+                    println!("inside");
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 impl FromStr for HailCloud {
@@ -94,16 +193,16 @@ impl FromStr for HailCloud {
 }
 impl Display for HailCloud {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for stone in &self.stones {
-            writeln!(f, "{}", stone)?
+        for (stone, line) in self.stones.iter().zip(self.stone_lines.iter()) {
+            writeln!(f, "{} -> {}", stone, line)?
         }
         Ok(())
     }
 }
-pub fn run(input: &str, _min_test: usize, _max_test: usize) -> usize {
+pub fn run(input: &str, min_test: f64, max_test: f64) -> usize {
     let hail = input.parse::<HailCloud>().unwrap();
     println!("{}", hail);
-    0
+    hail.count_intersections_in_test_area(min_test, max_test)
 }
 
 #[cfg(test)]
@@ -111,6 +210,6 @@ mod tests {
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input, 7, 27), 2);
+        assert_eq!(super::run(input, 7_f64, 27_f64), 2);
     }
 }
--- src/day_24/mod.rs
+++ src/day_24/mod.rs
@@ -5,7 +5,7 @@ pub fn run() {
     let input = include_str!("data.txt");
     println!(
         "day 24 a {}",
-        a::run(input, 200_000_000_000_000, 400_000_000_000_000)
+        a::run(input, 200_000_000_000_000_f64, 400_000_000_000_000_f64)
     );
     println!("day 24 b {}", b::run(input));
 }
