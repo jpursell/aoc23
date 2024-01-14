45aead670863dd09f5649394b19625d47902db5a
Jeff Pursell <jpursell@gmail.com>
2024-01-14 10:21:29 -0500

    Add day 24 data

--- src/day_24/a.rs
+++ src/day_24/a.rs
@@ -7,6 +7,6 @@ mod tests {
     #[test]
     fn test1() {
         let input = include_str!("example_data.txt");
-        assert_eq!(super::run(input), 0);
+        assert_eq!(super::run(input), 2);
     }
 }
