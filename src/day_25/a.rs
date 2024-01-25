8f764f6c45cb0fa4befea4dacda5299b0447be75
Jeff Pursell <jpursell@gmail.com>
2024-01-25 07:06:25 -0500

    Work on 25 a

--- src/day_25/a.rs
+++ src/day_25/a.rs
@@ -1,37 +1,56 @@
 use std::{
-    collections::{HashMap, HashSet},
-    str::FromStr, fmt::Display,
+    collections::{BTreeMap, HashMap},
+    fmt::Display,
+    str::FromStr,
 };
 
-struct Graph {
-    edges: HashMap<String, HashSet<String>>,
+use petgraph::{graph::UnGraph, stable_graph::IndexType, visit::{EdgeRef, NodeRef}, Graph, Undirected};
+
+struct AocGraph {
+    graph: Graph<u32, (), Undirected>,
+    node_map: HashMap<String, _>,
+    // node_num_to_str: BTreeMap<u32, String>,
 }
 
-impl FromStr for Graph {
+impl FromStr for AocGraph {
     type Err = ();
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut edges = HashMap::new();
+        todo!("trying to make a graph like here https://docs.rs/petgraph/latest/petgraph/graph/struct.Graph.html#");
+        let mut edges = Vec::new();
+        let mut node_map = HashMap::new();
+        let mut graph = Graph::<&str, &str>::new()
+
         for line in s.lines() {
             let (l_node, r_nodes) = line.split_once(": ").unwrap();
-            if !edges.contains_key(l_node) {
-                edges.insert(l_node.to_string(), HashSet::new());
+            if !node_str_to_num.contains_key(l_node) {
+                node_str_to_num.insert(l_node.to_string(), node_count);
+                node_num_to_str.insert(node_count, l_node.to_string());
+                node_count += 1;
             }
             for r_node in r_nodes.split(" ") {
-                edges.get_mut(l_node).unwrap().insert(r_node.to_string());
-                if !edges.contains_key(r_node) {
-                    edges.insert(r_node.to_string(), HashSet::new());
+                if !node_str_to_num.contains_key(r_node) {
+                    node_str_to_num.insert(r_node.to_string(), node_count);
+                    node_num_to_str.insert(node_count, r_node.to_string());
+                    node_count += 1;
                 }
-                edges.get_mut(r_node).unwrap().insert(l_node.to_string());
+                edges.push((node_str_to_num[l_node], node_str_to_num[r_node]));
             }
         }
-        Ok(Graph { edges })
+        let graph = UnGraph::<u32, ()>::from_edges(edges.iter());
+        Ok(AocGraph {
+            graph,
+            node_num_to_str,
+        })
     }
 }
 
-impl Display for Graph {
+impl Display for AocGraph {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (l_node, set) in &self.edges{
-            write!(f, "{}: ", l_node).unwrap();
+        for edge in self.graph.edges() {
+            let l_node = edge.source();
+            let l_node = l_node.index() as u32;
+            let l_node = self.node_num_to_str[&l_node];
+            write!(f, "{} ", l_node).unwrap();
             for r_node in set {
                 write!(f, "{} ", r_node).unwrap();
             }
@@ -41,16 +60,15 @@ impl Display for Graph {
     }
 }
 
-impl Graph {
+impl AocGraph {
     /// Find partition that has cost 3, return product of sizes
     fn run(&self) -> usize {
         todo!();
     }
-    
 }
 
 pub fn run(input: &str) -> usize {
-    let graph = input.parse::<Graph>().unwrap();
+    let graph = input.parse::<AocGraph>().unwrap();
     println!("{}", graph);
     graph.run()
 }
